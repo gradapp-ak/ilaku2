de35d52db9f3acf92593456ef11a20bfc73c28e1
panamara <karthick.ak@outlook.com>
2018-09-30 11:06:28 -0400

    DP - Matrix path problem; package arrangement

--- src/murai/dpAndrecursion/PowerSet.groovy
+++ src/murai/dpAndrecursion/PowerSet.groovy
@@ -1,4 +1,24 @@
 package murai.dpAndrecursion
 
 class PowerSet {
+
+    static void main(String[] args) {
+        def powerSet = new PowerSet()
+        List input = ['a', 'b', 'c']
+        def results = powerSet.generatePowerSet(input)
+        println("results=$results")
+    }
+
+    List<List> generatePowerSet(List input) {
+        List<List> result = [[]]
+
+        input.each { e ->
+            List out = []
+            result.each { l ->
+                out << l + [e]
+            }
+            result.addAll(out)
+        }
+        result
+    }
 }
