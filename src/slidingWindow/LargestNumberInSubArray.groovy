1ad8f69b4500eec3dc62cf8f07251b13b79c1c8b
panamara <karthick.ak@outlook.com>
2019-07-12 07:57:15 -0400

    Added a dequeue O(n)

--- src/slidingWindow/LargestNumberInSubArray.groovy
+++ src/slidingWindow/LargestNumberInSubArray.groovy
@@ -6,8 +6,32 @@ class LargestNumberInSubArray {
     static void main(String[] args) {
         findMaxBruteForce([1, 2, 3, 1, 4, 5, 2, 3, 6], 3)
         findMaxBruteForce([8, 5, 10, 7, 9, 4, 15, 12, 90, 13], 4)
+        findMaxDeque([1, 2, 3, 1, 4, 5, 2, 3, 6], 3)
+        findMaxDeque([8, 5, 10, 7, 9, 4, 15, 12, 90, 13], 4)
     }
 
+    //O(n)
+    static void findMaxDeque(List<Integer> elements, int k) {
+        Deque deque = new ArrayDeque<Integer>()
+        for (int i = 0; i < elements.size(); i++) {
+            if (deque.size() == k) {
+                deque.removeFirst()
+            }
+            if (deque.isEmpty() || deque.peekLast() > elements[i]) {
+                deque.addLast(elements[i])
+            } else {
+                while (!deque.isEmpty() && deque.peekLast() < elements[i]) {
+                    deque.removeLast()
+                }
+                deque.addLast(elements[i])
+            }
+            if(i >= k-1){
+                println("max so far, start=${i - k + 1},end=$i,max=${deque.peekFirst()}")
+            }
+        }
+    }
+
+    //O(n2)
     static void findMaxBruteForce(List<Integer> elements, int k) {
         for (int j = 0; j <= elements.size() - k; j++) {
             println("start=$j,end=${j + k},max=${findMax(j, j + k, elements)}")
